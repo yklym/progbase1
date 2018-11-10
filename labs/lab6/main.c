d1d8e8b654e3158c1705de9034ce575744151f4b
Yaroslaw Klymenko <yaroslaw.klymko@gmail.com>
2018-11-10 10:55:29 +0200

    lab 6 done

--- labs/lab6/main.c
+++ labs/lab6/main.c
@@ -48,15 +48,15 @@ int main()
     Console_clear();
     srand(time(0));
     struct ConsoleSize cs = Console_size();
+    
     const int w = cs.columns;
     const int h = cs.rows * 2;
     const int delay = 100;
-    char key = '\0';
-    const int pointAverage = rand() % (10) + 1;
-    const int ballAverage = 10;
+    const int gravitation = 2;
+    const int pointAverage = rand() % (5 - 1 + 1) + 1;
+    const int ballAverage = 5;
 
-
-    struct point points[pointAverage];   
+    struct point points[pointAverage];
     for (int i = 0; i < pointAverage; i++)
     {
         points[i].radius = 2;
@@ -67,80 +67,91 @@ int main()
         points[i].color.blue = randRGB();
         points[i].power = 0;
     }
-    struct Ball ball[ballAverage]; 
+    struct Ball ball[ballAverage];
     for (int i = 0; i < ballAverage; i++)
     {
         ball[i].radius = rand() % (5 + 1 - 3) + 3;
         ball[i].loc.x = w / 2;
         ball[i].loc.y = ball[i].radius + 1;
-        ball[i].bColor.red = randRGB(); 
+        ball[i].bColor.red = randRGB();
         ball[i].bColor.green = randRGB();
         ball[i].bColor.blue = randRGB();
         ball[i].speed.x = 0;
         ball[i].speed.y = 3;
     };
     int lich = 0;
-    
+
     Canvas_setSize(w, h);
     Canvas_invertYOrientation();
-    while (1)
+    do
     {
-        for (int i = 0; i < ballAverage; i++){
-        
-            for(int j = 0; j < pointAverage; j++){
-            
+        lich++;
+        for (int i = 0; i <= lich / 10; i++)
+        {
+            if( i == ballAverage){
+                break;
+            }
+            for (int j = 0; j < pointAverage; j++)
+            {
+
                 points[j].distance.x = ball[i].loc.x - points[j].loc.x;
                 points[j].distance.y = ball[i].loc.y - points[j].loc.y;
                 points[j].power = sqrt(pow(points[j].distance.x, 2) + pow(points[j].distance.y, 2));
-                //points[].power = sqrt(points[].loc.x * points[].loc.x + points[].loc.y * points[].loc.y);
+                //ball[i].speed.x += points[j].distance.x  * gravitation/ pow(points[j].power, 2);
                 if(points[j].loc.x > ball[i].loc.x){
-                    ball[i].speed.x -= points[j].distance.x / pow(points[j].power, 2);
+                    ball[i].speed.x += points[j].distance.x  * gravitation/ pow(points[j].power, 2);
                 }
                 if(points[j].loc.x < ball[i].loc.x){
-                    ball[i].speed.x += points[j].distance.x / pow(points[j].power, 2);
+                    ball[i].speed.x -= points[j].distance.x * gravitation / pow(points[j].power, 2);
                 }
                 if(points[j].loc.y > ball[i].loc.y){
-                    ball[i].speed.y -= points[j].distance.y / pow(points[j].power, 2);
+                    ball[i].speed.y += points[j].distance.y * gravitation / pow(points[j].power, 2);
                 }
                 if(points[j].loc.y < ball[i].loc.y){
-                    ball[i].speed.y += points[j].distance.y / pow(points[j].power, 2);
+                    ball[i].speed.y -= points[j].distance.y * gravitation / pow(points[j].power, 2);
                 }
             }
-        
-            ball[i].loc.x += ball[i].speed.x;
-            ball[i].loc.y += ball[i].speed.y;
-            
-            if(ball[i].loc.x + ball[i].radius >= w -1 || ball[i].loc.x - ball[i].radius <= 1){
+
+            if (ball[i].loc.x + ball[i].radius >= w || ball[i].loc.x - ball[i].radius <= 0)
+            {
                 ball[i].speed.x = -ball[i].speed.x;
             }
-            if(ball[i].loc.y + ball[i].radius >= h - 1 || ball[i].loc.y - ball[i].radius <= 2 ){
+            if (ball[i].loc.y + ball[i].radius >= h || ball[i].loc.y - ball[i].radius <= 0)
+            {
                 ball[i].speed.y = -ball[i].speed.y;
             }
-            if(ball[i].loc.x == 0 && ball[i].loc.y == 0){
+            if (ball[i].speed.x == 0 || ball[i].speed.y == 0)
+            {
                 ball[i].speed.x += 3;
                 ball[i].speed.y += 3;
             }
+
+            ball[i].loc.x += ball[i].speed.x;
+            ball[i].loc.y += ball[i].speed.y;
+            
+        }
+
+        Canvas_beginDraw();
+        for (int i = 0; i <= lich; i += 10)
+        {
+            if(i / 10 == ballAverage){
+                break;
+            }
+            Canvas_setColorRGB(ball[(i / 10)].bColor.red, ball[i / 10].bColor.green, ball[i / 10].bColor.blue);
+            Canvas_fillCircle(ball[i / 10].loc.x, ball[i / 10].loc.y, ball[i / 10].radius);
+            
+        }
+        for (int k = 0; k < pointAverage; k++)
+        {
+            Canvas_setColorRGB(points[k].color.red, points[k].color.green, points[k].color.blue);
+            Canvas_strokeCircle(points[k].loc.x, points[k].loc.y, 2);
         }
         
-        Canvas_beginDraw();
-        for(int i = 0; i <= lich; i+=10){
-            Canvas_setColorRGB(ball[(i/10)].bColor.red, ball[i/10].bColor.green, ball[i/10].bColor.blue);
-            Canvas_fillCircle(ball[i/10].loc.x, ball[i/10].loc.y, ball[i/10].radius);
-        }
-        lich++;
-        for(int k = 0; k < pointAverage; k++){
-            Canvas_setColorRGB(points[k].color.red,points[k].color.green, points[k].color.blue);
-            Canvas_strokeCircle(points[k].loc.x, points[k].loc.y, 2);   
-        }
-        // for (int i = 0; i < i++){
-        //     Canvas_setColorRGB(points[].color.red, points[].color.green, points[].color.blue);
-        //     Canvas_strokeCircle(points[].loc.x, points[].loc.y, points[].radius);   
-        // }
-        //
         Canvas_endDraw();
 
         sleepMillis(delay);
-    }
+        
+    } while (!Console_isKeyDown());
 
     return 0;
 }
